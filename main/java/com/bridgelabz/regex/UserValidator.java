09db035dbff0b6f5894335234b22c25e281121c0
Roshni1998 <roshnimali847@gmail.com>
2021-06-22 00:27:52 +0530

    Added: Welcome Message

---
+++ main/java/com/bridgelabz/regex/UserValidator.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.regex;
+
+import java.util.regex.Pattern;
+    /**
+     * @author ROSHNI
+     * User Registration Problem
+     */
+
+    public class UserValidator {
+
+        public static void main(String[] args){
+            System.out.println("Welcome to User Validator UC-9");
+        }
+    }
