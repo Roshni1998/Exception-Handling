0d72e46650f118f220ff8203a86e37859b374c4f
Roshni1998 <roshnimali847@gmail.com>
2021-06-22 11:01:10 +0530

    Added: User Registration UC-11

--- main/java/com/bridgelabz/regex/UserValidator.java
+++ main/java/com/bridgelabz/regex/UserValidator.java
@@ -8,77 +8,17 @@ import java.util.regex.Pattern;
 
 public class UserValidator {
 
-    /*UC-1*/
-    //First name starts with Cap and has minimum 3 characters
-    public String isValidFirstName(String fname) {
-        /**
-         * @param firstName
-         * @return boolean
-         */
-        String regex = "^[A-Z][a-z]{3,15}$";
-        if (fname.matches(regex) == true)
-            return "HAPPY";
-        else
-            return "SAD";
-    }
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([.+-_][0-9a-zA-Z]+)*"+"@([a-zA-Z0-9][-]?)+[.][a-zA-Z]{2,4}+([.][a-zA-Z]{0,3})?$";
 
-    /*UC-2*/
-    // Last name starts with Cap and has minimum 3 characters
-    public String isValidLastName(String lname) {
-        /**
-         *
-         * @param lastName
-         * @return boolean
-         */
-        String regex = "^[A-Z][a-z]{3,15}$";
-        if (lname.matches(regex) == true)
-            return "HAPPY";
-        else
-            return "SAD";
-    }
-//    /*UC-3*/
-//    //Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
-    public String isValidEmailId(String emailId) {
+    /*UC-9*/
+    //Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
+    public boolean isValidEmailId(String emailId) {
         /**
          *
          * @param emailId
          * @return boolean
          */
-        String regex = "^[a-zA-Z0-9]+([.+-_][0-9a-zA-Z]+)*"+"@([a-zA-Z0-9][-]?)+[.][a-zA-Z]{2,4}+([.][a-zA-Z]{0,2})?$";
-        if (emailId.matches(regex) == true)
-            return "HAPPY";
-        else
-            return "SAD";
-    }
-//    /*UC-4*/
-//    //Country code follow by space and 10 digit number
-    /**
-     *
-     * @param mobileNo
-     * @return boolean
-     */
-    public String isValidMobileNo(String mobileNo) {
-        String regex = "(0|91)?\\s([7-9][0-9]{9})";
-        if (mobileNo.matches(regex) == true)
-            return "HAPPY";
-        else
-            return "SAD";
-    }
-//    /*UC-5, UC-6, UC-7, UC-8*/
-    /* Password should contain minimum 8 Characters
-     * Password should have at least 1 Upper Case
-     * Password should have at least 1 numeric number
-     * Should have exactly 1 Special Character
-     */
-    /**
-     * @param password
-     * @return boolean
-     */
-    public String isValidPassword(String password) {
-        String regex = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&-+=()]).{8,12}$";
-        if (password.matches(regex) == true)
-            return "HAPPY";
-        else
-            return "SAD";
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(emailId). matches();
     }
 }
